e2310b76884d125d7cb305e3d3861e00335c39c1
Romm1002 <78146244+Romm1002@users.noreply.github.com>
2024-09-05 10:34:52 +0200

    crud react (#2)

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import AddProduct from "./screens/AddProduct";
+import EditProduct from "./screens/EditProduct";
+import DetailsProduct from "./screens/DetailsProduct";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="AddProduct"
+        screenOptions={{ headerShown: true }}
+      >
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        <Stack.Screen
+          name="AddProduct"
+          component={AddProduct}
+          options={{ title: "Ajouter un produit" }}
+        />
+
+        <Stack.Screen
+          name="EditProduct"
+          component={EditProduct}
+          options={{ title: "Editer un produit" }}
+        />
+
+        <Stack.Screen
+          name="DetailsProduct"
+          component={DetailsProduct}
+          options={{ title: "Détails d'un produit" }}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ screens/EditProduct.js
@@ -0,0 +1,153 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import axios from "axios";
+
+const EditProduct = ({ navigation }) => {
+  const route = useRoute();
+  const [product, setProduct] = useState(null);
+  const [productInput, setProductInput] = useState({
+    id: "",
+    name: "",
+    description: "",
+    quantity: "",
+    price: "",
+  });
+  const { productId } = route.params;
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(
+          "http://192.168.1.20:5000/items/" + productId
+        );
+        setProduct(response.data);
+        setProductInput(response.data);
+      } catch (error) {
+        console.log(
+          "Une erreur est survenue lors de la récupération du produit",
+          error
+        );
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  const handleChange = (key, value) => {
+    setProductInput({ ...productInput, [key]: value });
+  };
+
+  const updateProduct = async () => {
+    try {
+      const response = await axios.put(
+        "http://192.168.1.20:5000/items/" + productId,
+        bookInput
+      );
+      setBook(response.data);
+    } catch (error) {
+      console.log(
+        "Une erreur est survenue lors de la mise a jour du produit",
+        error
+      );
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Mettre à jour le produit</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nom"
+        value={productInput.name}
+        onChangeText={(value) => handleChange("name", value)}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Description"
+        value={productInput.description}
+        onChangeText={(value) => handleChange("description", value)}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Quantité"
+        value={productInput.quantity}
+        onChangeText={(value) => handleChange("quantity", value)}
+        keyboardType="numeric"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Prix"
+        value={productInput.price}
+        onChangeText={(value) => handleChange("price", value)}
+      />
+
+      <Button title="Mise à jour" onPress={updateProduct} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  templateAuthor: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingVertical: 16,
+  },
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  bookItem: {
+    flexDirection: "row",
+    marginBottom: 16,
+    backgroundColor: "#f8f8f8",
+    padding: 10,
+    borderRadius: 8,
+  },
+  cover: {
+    width: 60,
+    height: 90,
+    marginRight: 16,
+  },
+  bookDetails: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  author: {
+    fontSize: 14,
+    color: "#666",
+    marginBottom: 8,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  addButton: {
+    backgroundColor: "#007bff",
+    padding: 15,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  addButtonText: {
+    color: "#fff",
+    fontSize: 18,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    marginBottom: 10,
+  },
+});
+
+export default EditProduct;
